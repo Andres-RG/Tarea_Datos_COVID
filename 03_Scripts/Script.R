14d9fe76e14021919977145c75aa321ed33cee88
Andres-RG <lrojas26@alumnos.uaq.mx>
2021-11-03 04:49:35 -0600

    Avance de tarea

--- 03_Scripts/Script.R
+++ 03_Scripts/Script.R
@@ -242,3 +242,82 @@ hospitalizados_totales
 jpeg("hospitalizados totales.jpeg", width = 750, height = 350)
 hospitalizados_totales
 dev.off()
+
+
+
+
+
+
+
+# 8. ¿Cuál es la probabilidad de que individuos positivos a covid sin comorbilidades sean hospitalizados por rangos de edades en adultos (18-29,30-39,40-49,50-59,60-70, 70+)?
+
+conmor <- select(datos_covid_gto, c(EDAD, CLASIFICACION_FINAL, TIPO_PACIENTE, DIABETES, 
+                                  EPOC, ASMA, INMUSUPR, HIPERTENSION, OTRA_COM,
+                                  CARDIOVASCULAR, OBESIDAD, RENAL_CRONICA, TABAQUISMO,
+                                  OTRO_CASO, FECHA_INGRESO))
+
+wo_conmor <- filter(conmor, DIABETES & EPOC & INMUSUPR & HIPERTENSION
+                 & OTRA_COM & CARDIOVASCULAR & OBESIDAD & RENAL_CRONICA & TABAQUISMO
+                 & OTRO_CASO == 2 & CLASIFICACION_FINAL == 1 | CLASIFICACION_FINAL == 3 &
+                   TIPO_PACIENTE != 2)
+
+wo_t_conmor <- filter(conmor, DIABETES & EPOC & INMUSUPR & HIPERTENSION
+                      & OTRA_COM & CARDIOVASCULAR & OBESIDAD & RENAL_CRONICA & TABAQUISMO
+                      & OTRO_CASO == 2 & CLASIFICACION_FINAL == 1 | CLASIFICACION_FINAL == 3)
+
+e_18_29 <- filter(wo_conmor, EDAD >= 18 & EDAD <= 29)
+e_18_29 <- mutate(e_18_29, rango_edad = "18-29")
+e_t_18_29 <- filter(wo_t_conmor, EDAD >= 18 & EDAD <= 29)
+p_18_29 <- length(e_18_29$TIPO_PACIENTE)/length(e_t_18_29$TIPO_PACIENTE)*100
+e_18_29 <- mutate(e_18_29, p= p_18_29)
+
+e_30_39 <- filter(wo_conmor, EDAD >= 30 & EDAD <= 39)
+e_30_39 <- mutate(e_30_39, rango_edad = "30-39")
+e_t_30_39 <- filter(wo_t_conmor, EDAD >= 18 & EDAD <= 29)
+p_30_39 <- length(e_30_39$TIPO_PACIENTE)/length(e_t_30_39$TIPO_PACIENTE)*100
+e_30_39 <- mutate(e_30_39, p= p_30_39)
+
+e_40_49 <- filter(wo_conmor, EDAD >= 40 & EDAD <= 49)
+e_40_49 <- mutate(e_40_49, rango_edad = "40-49")
+e_t_40_49 <- filter(wo_t_conmor, EDAD >= 18 & EDAD <= 29)
+p_40_49 <- length(e_40_49$TIPO_PACIENTE)/length(e_t_40_49$TIPO_PACIENTE)*100
+e_40_49 <- mutate(e_40_49, p= p_40_49)
+
+e_50_59 <- filter(wo_conmor, EDAD >= 50 & EDAD <= 59)
+e_50_59 <- mutate(e_50_59, rango_edad = "50-59")
+e_t_50_59 <- filter(wo_t_conmor, EDAD >= 18 & EDAD <= 29)
+p_50_59 <- length(e_50_59$TIPO_PACIENTE)/length(e_t_50_59$TIPO_PACIENTE)*100
+e_50_59 <- mutate(e_50_59, p= p_50_59)
+
+e_60_69 <- filter(wo_conmor, EDAD >= 60 & EDAD <= 69)
+e_60_69 <- mutate(e_60_69, rango_edad = "60-69")
+e_t_60_69 <- filter(wo_t_conmor, EDAD >= 18 & EDAD <= 29)
+p_60_69 <- length(e_60_69$TIPO_PACIENTE)/length(e_t_60_69$TIPO_PACIENTE)*100
+e_60_69 <- mutate(e_60_69, p= p_60_69)
+
+e_70 <- filter(wo_conmor, EDAD >= 70)
+e_70 <- mutate(e_70, rango_edad = "+70")
+e_t_70 <- filter(wo_t_conmor, EDAD >= 18 & EDAD <= 29)
+p_70 <- length(e_70$TIPO_PACIENTE)/length(e_t_70$TIPO_PACIENTE)*100
+e_70 <- mutate(e_70, p= p_70)
+
+
+re_wo_conmor <- rbind(e_18_29, e_30_39, e_40_49, e_50_59, e_60_69, e_70)
+
+plot_p_hosp <- ggplot(re_wo_conmor, aes(x=rango_edad, y=p,fill=rango_edad)) +
+  geom_bar(position="stack", stat="identity") +
+  ggtitle("Probabilidad de hospitalización para personas sin conmorbilidades de individuos positivos a COVID por rango de edad para el estado de Guanajuato") + 
+  labs(x="Rangos de edad", y="Probabilidad") +
+  labs(fill="Rangos de Edad") +
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(panel.background = element_rect(fill = "white"), 
+        axis.line = element_line(colour = "black", size = 1)) +
+  scale_fill_viridis(discrete = T) 
+
+plot_p_hosp
+
+jpeg("probabilidad de hospitalizacion.jpeg", width = 950, height = 350)
+plot_p_hosp
+dev.off()
+
+
